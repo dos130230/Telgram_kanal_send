817c851e83db5c9a13a92be96995f9418dcc47bc
Dostonbek O'ktamov <88940496+dos130230@users.noreply.github.com>
2022-02-28 23:41:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const token = '5129439956:AAFSdszc_FXbh07mhhrQfVHTGcjR6wmvIz4'
+const chat_id = -1001569041413
+const api = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=`
+
+
+errorMessage.textContent = null
+form.onsubmit = (event) => {
+    try {
+        event.preventDefault()
+        if (!fname.value || !lname.value || !country.value || !subject.value) throw new Error("Forma toldirsh shart!")
+        if (fname.value.length > 20) throw new Error("Ism uzunligi max 20")
+        if (lname.value.length > 20)  throw new Error("Familya uzunligi max 20")
+        if (subject.value.length > 5000)  throw new Error("Subject uzunligi max 5000")
+            let message = `-Ism: ${fname.value} %0A%0A-Familyasi: ${lname.value} %0A%0A-Mamlakat : ${country.value}%0A%0A-Qo'shimcha malumot : ${subject.value}`
+        let res = fetch(api + message)
+        fname.value = null
+        fname.lname = null
+        fname.country = null
+        fname.subject = null
+    } catch (error) {
+        errorMessage.textContent = error.message
+    }
+}
