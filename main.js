dd5672037b89965413ea391ba39aa63c6773ba09
Dostonbek O'ktamov <88940496+dos130230@users.noreply.github.com>
2022-03-01 17:29:55 +0500

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,11 @@ form.onsubmit = (event) => {
         if (fname.value.length > 20) throw new Error("Ism uzunligi max 20")
         if (lname.value.length > 20)  throw new Error("Familya uzunligi max 20")
         if (subject.value.length > 5000)  throw new Error("Subject uzunligi max 5000")
-            let message = `${fname.value} %0A%0A${lname.value} %0A%0A${country.value} %0A%0A${subject.value}`
+        let message = `${fname.value} %0A%0A${lname.value} %0A%0A${country.value} %0A%0A${subject.value}`
         let text = fetch(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=`+ message)
         fname.value = null
-        fname.lname = null
-        fname.country = null
-        fname.subject = null
+        lname.value = null
+        subject.value = null
     } catch (error) {
         errorMessage.textContent = error.message
     }
